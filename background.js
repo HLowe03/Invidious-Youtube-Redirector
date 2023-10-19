b3c8f9f746a0782c5683965d6217552e017f6cf4
HLowe03 <harrisonlowe03@gmail.com>
2023-10-19 11:59:19 +0100

    Cut down on code

--- background.js
+++ background.js
@@ -1,17 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("hello")
-
-  //receiving a message
-  chrome.runtime.onMessage.addListener(
-      function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.greeting === "hello")
-          sendResponse({farewell: "goodbye"});
-      }
-  );
-
   //create context menu
   chrome.contextMenus.create({
       id: "wikipedia",
