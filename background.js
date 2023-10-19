9f7e8b63029e1a4ea99fd90ed3be61c689e0092b
HLowe03 <harrisonlowe03@gmail.com>
2023-10-19 02:12:23 +0100

    Working

--- background.js
+++ background.js
@@ -15,16 +15,18 @@ chrome.runtime.onInstalled.addListener(() => {
   //create context menu
   chrome.contextMenus.create({
       id: "wikipedia",
-      title: "Search for: \"%s\" on Wikipedia", 
-      contexts: ["selection"], 
+      title: "Open Video in indivous", 
+      contexts: ["link"], 
   })
 });
 
 //listener for context menu
 chrome.contextMenus.onClicked.addListener(function(info, tab){
   //the URL that will be added to based on the selection
-  baseURL = "http://en.wikipedia.org/wiki/";
-  var newURL = baseURL + info.selectionText;
+
+  baseURL = "https://vid.puffyan.us/";
+  videoURL = info.linkUrl
+  var newURL = baseURL + videoURL.slice(24); // remove "https://www.youtube.com/" to just get "watch?v=whatever"
   //create the new URL in the user's browser
   chrome.tabs.create({ url: newURL });
 })
