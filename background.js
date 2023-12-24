5c203728bd0d04a34f60fcfd7df52a5eaab1334d
hlowe03 <harrisonlowe03@gmail.com>
2023-12-24 22:50:29 +0000

    Issue with asnyc

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
+  // When clickd on URL this works
   chrome.contextMenus.create({
     id: "linkContext",
     title: "Open video in Indivous",
@@ -6,11 +7,12 @@ chrome.runtime.onInstalled.addListener(() => {
     targetUrlPatterns: ["https://www.youtube.com/watch*"]
   });
 
+  // When clicked on tab, this runs
   chrome.contextMenus.create({
     id: "tabContext",
     title: "Open current tab in Indivous",
     contexts: ["page"],
-    targetUrlPatterns: ["https://www.youtube.com/watch*"]
+    documentUrlPatterns: ["https://www.youtube.com/watch*"]
   });
 });
 
@@ -19,15 +21,16 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
   // The URL that will be added to based on the selection
 
-  const baseURL = "https://vid.puffyan.us/";
+  var baseURL = "https://vid.puffyan.us/";
   let videoURL = "";
+  var newURL = ""
 
   if (info.menuItemId == "linkContext") {
     // If clicked on link
     videoURL = info.linkUrl;
 
-    var newURL = baseURL + videoURL.slice(24); // Remove "https://www.youtube.com/" to just get "watch?v=whatever"
-    chrome.tabs.create({ url: newURL });
+    newURL = baseURL + videoURL.slice(24); // Remove "https://www.youtube.com/" to just get "watch?v=whatever"
+    //chrome.tabs.create({ url: newURL });
   } 
 
   else if (info.menuItemId == "tabContext") {
@@ -40,11 +43,17 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
 
         if (videoURL) {
           console.log(videoURL)
-          const newURL = baseURL + videoURL.slice(24); // Remove "https://www.youtube.com/" to get "watch?v=whatever"
-          // Create the new URL in the user's browser
-          chrome.tabs.create({ url: newURL });
+          newURL = baseURL + videoURL.slice(24); // Remove "https://www.youtube.com/" to get "watch?v=whatever"
         }
       }
     })
   }
+
+
+  // to delay so async func (tabs.query) can run if clicked on page
+  while (newURL == "") { 
+    // pass
+    console.log("Stuck")
+  }
+  chrome.tabs.create({ url: newURL });
 })
